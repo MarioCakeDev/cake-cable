5d3fd19f4c9a5c597c3e903d6a748706c58b45fa
alexa <alex@trirecords.eu>
2023-03-17 10:23:12 +0100

    Add item group, cable to group + custom icon

--- src/main/java/de/mariocake/cakecable/CakeCableMod.java
+++ src/main/java/de/mariocake/cakecable/CakeCableMod.java
@@ -2,10 +2,13 @@ package de.mariocake.cakecable;
 
 import de.mariocake.cakecable.common.CableBlock;
 import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
@@ -25,11 +28,20 @@ public class CakeCableMod implements ModInitializer {
 
 	public static final CableBlock CABLE_BLOCK = new CableBlock(QuiltBlockSettings.of(Material.GLASS).hardness(0.3f));
 
+	private static final ItemGroup CAKECABLE_ITEMGROUP = FabricItemGroup.builder(new Identifier("cakecable", "general"))
+			.icon(() -> new ItemStack(CABLE_BLOCK))
+			.name(Text.of("CakeCable"))
+			.build();
+
 	@Override
 	public void onInitialize(ModContainer mod) {
 		Registry.register(Registries.BLOCK, CableBlock.IDENTIFIER, CABLE_BLOCK);
-		Registry.register(Registries.ITEM, CableBlock.IDENTIFIER, new BlockItem(CABLE_BLOCK, new QuiltItemSettings()));
+		BlockItem BLOCK_ITEM = new BlockItem(CABLE_BLOCK, new QuiltItemSettings().maxCount(64));
+		Registry.register(Registries.ITEM, CableBlock.IDENTIFIER, BLOCK_ITEM);
 
+		ItemGroupEvents.modifyEntriesEvent(CAKECABLE_ITEMGROUP).register(content -> {
+			content.addItem(BLOCK_ITEM);
+		});
 
 		// Add event handler if all mods are loaded
 		CommonLifecycleEvents.TAGS_LOADED.register((dynamicRegistryManager, bool) -> {
